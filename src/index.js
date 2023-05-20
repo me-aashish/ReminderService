58be348da36eb219a99b5e9c090da6b86126dd01
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-05-21 03:18:54 +0530

    set up of mailing service using nodemailer done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service')
+
 const setUpAndStartServer = () => {
     const app = express();
 
@@ -12,6 +14,12 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server Started on Port : ${PORT}`);
+        sendBasicEmail(
+            'support@admin.com',
+            'jha.aashish11@gmail.com',
+            'This is the subject of testing email',
+            'Hey! How are you ? This is a testing email'
+        )
     })
 }
 
